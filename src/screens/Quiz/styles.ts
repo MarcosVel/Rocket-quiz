fe50658e9de2000d2c45c39e1f4b611fc6baa756
MarcosVel <marcosvelosodev@gmail.com>
2024-06-21 02:27:23 -0300

    feat: styles for animated header

--- src/screens/Quiz/styles.ts
+++ src/screens/Quiz/styles.ts
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native';
-import { THEME } from '../../styles/theme';
+import { StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
@@ -8,13 +8,24 @@ export const styles = StyleSheet.create({
   },
   question: {
     flexGrow: 1,
-    alignItems: 'center',
+    alignItems: "center",
     paddingTop: 80,
     paddingBottom: 64,
     padding: 32,
   },
   footer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 24,
-  }
+  },
+  title: {
+    fontFamily: THEME.FONTS.BOLD,
+    color: THEME.COLORS.GREY_100,
+    fontSize: 16,
+    marginBottom: 8,
+    textAlign: "center",
+  },
+  header: {
+    width: "100%",
+    marginBottom: 20,
+  },
 });
